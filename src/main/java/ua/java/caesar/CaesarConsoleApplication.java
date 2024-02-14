c963ee4c936071696c15be05177cc42c9a57aa47
Yegor Popinako <yegor5x@gmail.com>
2024-02-14 20:20:04 +0100

    test commit from new device

--- src/main/java/ua/java/caesar/CaesarConsoleApplication.java
+++ src/main/java/ua/java/caesar/CaesarConsoleApplication.java
@@ -7,6 +7,7 @@ import ua.java.caesar.view.ConsoleViewProvider;
 
 public class CaesarConsoleApplication {
     public static void main(String[] args) {
+        int C = 2;
         FileProcessor fileProcessor = new FileProcessor(new CaesarCypher());
         ConsoleViewProvider consoleViewProvider = new ConsoleViewProvider();
         new ApplicationController(fileProcessor, consoleViewProvider).execute(args);
